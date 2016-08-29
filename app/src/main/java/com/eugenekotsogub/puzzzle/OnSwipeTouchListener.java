63bb675290864f78e3b65aab01c3582b1e1d89e7
Eugene Kotsogub <eugene.kotsogub@idapgroup.com>
2016-08-29 17:41:35 +0300

    now swipe to move

---
+++ app/src/main/java/com/eugenekotsogub/puzzzle/OnSwipeTouchListener.java
@@ -0,0 +1,71 @@
+package com.eugenekotsogub.puzzzle;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public abstract class OnSwipeTouchListener implements OnTouchListener {
+
+    private final GestureDetector gestureDetector;
+    View view;
+
+    public OnSwipeTouchListener (Context ctx){
+        gestureDetector = new GestureDetector(ctx, new GestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        view = v;
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    private final class GestureListener extends SimpleOnGestureListener {
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight(view);
+                        } else {
+                            onSwipeLeft(view);
+                        }
+                    }
+                    result = true;
+                }
+                else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom(view);
+                    } else {
+                        onSwipeTop(view);
+                    }
+                }
+                result = true;
+
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+    }
+
+    public abstract void onSwipeRight(View view);
+    public abstract void onSwipeLeft(View view);
+    public abstract void onSwipeTop(View view);
+    public abstract void onSwipeBottom(View view);
+}
